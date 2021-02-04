da73196ab792dbc11f5404b5fd92bf54742e9558
Alex <Alex13f13@gmail.com>
2021-02-04 12:44:53 +0100

    Pre-Beta

---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Tutorial : MonoBehaviour
+{
+    public GameObject ui;
+
+	private void Start()
+	{
+        StartCoroutine(Wait());
+    }
+
+    #region Funciones
+
+    public void Next()
+	{
+        Time.timeScale = 1f;
+        ui.SetActive(false);
+    }
+
+    IEnumerator Wait()
+	{
+        yield return new WaitForSeconds(0.5f);
+        Time.timeScale = 0f;
+    }
+
+    #endregion
+}
